e35a2ac521783043e9cf8f3701eea2c2ae570343
MohamedAnwar7 <mohamedbatal07@gmail.com>
2019-08-25 17:49:54 +0200

    initial commit

--- Tuples.py
+++ Tuples.py
@@ -7,13 +7,13 @@ print(tuple_2)
 # print(tuple_2)
 
 # sets
-#empty set
+# empty set
 empty_set = set()
-set_1 = {'hi', 'migo', 'flor','what'}
+set_1 = {'hi', 'migo', 'flor', 'what'}
 set_2 = {'hi', 'migo', 'flor', 'mora'}
 print(set_1.intersection(set_2))
 print(set_2.difference(set_1))
 print(set_1.union(set_2))
 
 # empty list = [] or list()
-#empty tuple = () or tuple ()
+# empty tuple = () or tuple ()
--- fileOP.py
+++ fileOP.py
@@ -1,7 +1,17 @@
 # file operations
+import os
 
-f = open('test.txt', 'r')
+# f = open('test.txt', 'r')
+# print(f.name)
+# print(f.mode) >> return file open type
+# f.close()
+with open('test.txt', 'r') as rf:
+    with open('test2.txt', 'w') as wf:
+        for line in rf:
+            wf.write(line)
 
-print(f.name)
-
-f.close()
+# copy image file 
+with open('theres-no-place-like-localhost-51-1600x900.jpg', 'rb') as rf:
+    with open('copy_test.jpg', 'wb') as wf:
+        for line in rf:
+            wf.write(line)
